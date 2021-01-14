4663aa7f87eacede7e4e824d1d976d501d713985
Olivira-27 <oliviraadi@gmail.com>
2021-01-14 20:06:57 +0700

    Pratikum 2

---
+++ src/pra2/Pra2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pra2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pra2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 4;
+       for (int b = 1; b <= a; b++){
+           for (int c = 1; c <= b; c++) {
+               System.out.print('@');
+           }
+           System.out.println();
+       }
+   
+    }
+}
+    
+
